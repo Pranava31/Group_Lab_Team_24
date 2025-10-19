33e777ef44ae930571a89d1822183de2d125d2f8
darbanshreya <darban.s@northeastern.edu>
2025-10-19 15:26:53 -0400

    Added Member 2 Shreya

--- Info5100-University-Example/src/info5100/university/example/Member/Member_Shreya.java
+++ Info5100-University-Example/src/info5100/university/example/Member/Member_Shreya.java
@@ -9,5 +9,7 @@ package info5100.university.example.Member;
  * @author HP
  */
 public class Member_Shreya {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello from Member 2 Shreya Darban!");
+    }
 }
